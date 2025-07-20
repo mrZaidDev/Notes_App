d2d2fc71cd9119d8c7c50c738795157354a4ae24
Zaid Khan <zaidcodingtopcoding@gmail.com>
2025-07-20 09:56:14 +0500

    completed the backend

--- Backend/controllers/notesController.js
+++ Backend/controllers/notesController.js
@@ -1,5 +1,5 @@
 import notesModel from "../models/notesModel.js";
-
+import mongoose from "mongoose";
 // POSTING THE NOTE
 export const postingNote = async (req, res) => {
   const { title, content } = req.body;
@@ -11,7 +11,68 @@ export const postingNote = async (req, res) => {
     await notesModel.create({ title, content, user: userId });
     return res.status(201).json({ message: "note created successfully" });
   } catch (error) {
-    return res.status(500).json({message:'Internal server error'})
+    return res.status(500).json({ message: "Internal server error" });
   }
 };
-// 
+// GET ALL NOTES By specific user
+export const gettingNotes = async (req, res) => {
+  const userId = req.user.id;
+  try {
+    const allNotes = await notesModel.find({ user: userId });
+    return res.status(200).json(allNotes);
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+// DELETING THE NOTE
+export const deletingNote = async (req, res) => {
+  const noteId = req.params.id;
+  const userId = req.user.id;
+  if (!mongoose.Types.ObjectId.isValid(noteId)) {
+    return res.status(400).json({ message: "note id in not valid" });
+  }
+  try {
+    const foundNote = await notesModel.findById(noteId);
+    if (!foundNote) {
+      return res.status(400).json({ message: "note not found" });
+    }
+    if (userId != foundNote.user.toString()) {
+      return res
+        .status(403)
+        .json({ message: "forbidden! don't interrupt in others notes" });
+    }
+    await notesModel.deleteOne({ _id: foundNote._id });
+    return res.status(200).json({ message: "note deleted" });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+// EDITING THE NOTE
+export const updatingNote = async (req, res) => {
+  const noteId = req.params.id;
+  const userId = req.user.id;
+  if (!mongoose.Types.ObjectId.isValid(noteId)) {
+    return res.status(400).json({ message: "note id in not valid" });
+  }
+  try {
+    const foundNote = await notesModel.findById(noteId);
+    if (!foundNote) {
+      return res.status(404).json({ message: "note not found" });
+    }
+    if (userId != foundNote.user.toString()) {
+      return res
+        .status(403)
+        .json({ message: "forbidden! don't interrupt in others notes" });
+    }
+    const {title,content} = req.body
+    if(!title && !content) {
+        return res.status(400).json({message:"title or content is not provided"})
+    }
+    if(title) foundNote.title = title
+    if(content) foundNote.content = content
+    await foundNote.save()
+    return res.status(200).json({message:"note Updated"})
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
--- Backend/routes/notesRoute.js
+++ Backend/routes/notesRoute.js
@@ -1,6 +1,10 @@
 import express from 'express'
 const router = express.Router()
-import {postingNote} from '../controllers/notesController.js'
+import {postingNote,gettingNotes,deletingNote,updatingNote} from '../controllers/notesController.js'
 
 router.post('/notes',postingNote)
+router.get('/notes',gettingNotes)
+router.delete('/notes/:id',deletingNote)
+router.put('/notes/:id',updatingNote)
+
 export default router
