44d83d1cf5ad11b28b8e7f64a763c2b2f3b460cf
Zaid Khan <zaidcodingtopcoding@gmail.com>
2025-07-26 12:05:22 +0500

    deleted the frontend

--- Frontend/src/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import authSlice from './features/myauth/authSlice'
-
-export const store = configureStore({
-  reducer: {
-    // Add your slices here
-    auth:authSlice
-  },
-});
--- Frontend/src/axios.jsx
+++
@@ -1,9 +0,0 @@
-// src/axios.js
-import axios from 'axios';
-
-const axiosInstance = axios.create({
-  baseURL: "/api", // No domain needed
-  withCredentials: true,
-});
-
-export default axiosInstance;
